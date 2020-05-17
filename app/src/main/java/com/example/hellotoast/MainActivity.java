ade5c1de3a2d9ba4f22fe2ed04313ce8a47fbdef
Bob Cohen <bobcohen1969@yahoo.com>
2020-05-17 13:51:57 -0500

    Hello Toast Lesson 2.1 Homework Part Added new activity and sending data to it

--- app/src/main/java/com/example/hellotoast/MainActivity.java
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.hellotoast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,17 +15,29 @@ public class MainActivity extends AppCompatActivity {
 
     private int mCount = 0;
     private TextView mShowCount;
+    public static final String EXTRA_MESSAGE = "com.example.hellotoast.SayHello.extra.MESSAGE";
+    private TextView numberCount;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mShowCount = (TextView) findViewById(R.id.show_count);
+       mShowCount = (TextView) findViewById(R.id.show_count);
+       numberCount = findViewById(R.id.button_count);
     }
 
     public void showToast(View view) {
         Toast toast = Toast.makeText(this, R.string.toast_message,Toast.LENGTH_SHORT);
         toast.show();
+    }
+
+    public void showCount(View view){
+        Intent intent = new Intent(this, SayHello.class);
+        String message = Integer.toString(mCount);
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
     }
 
     public void countUp(View view) {
